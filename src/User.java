fc3b9a7a7001b083e6f0cb968bc2beebf39d4d6b
Evan <evan.pittman@keyin.com>
2024-10-30 16:08:13 -0230

    added User functionality

--- src/User.java
+++ src/User.java
@@ -1,4 +1,42 @@
 public class User {
     private String name;
+    private TaskList taskList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+
+    public void printTask(){
+        taskList.printTaskList();
+    }
+
+    public void addTask(String description,int location){
+        taskList.addToTaskList(description,location);
+    }
+
+    public void markCompleted(String description){
+        taskList.markCompleted(description);
+    }
+
+
+
 
 }
